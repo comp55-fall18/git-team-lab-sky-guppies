a4532aea69015003d05af40d430ffb703b4d6db0
Osvaldo Jimenez <ojimenez@pacific.edu>
2017-03-06 11:38:43 -0800

    Trying to do a simple github project

---
+++ COMP55GitPrepLab/src/SimplePicture.java
@@ -0,0 +1,16 @@
+import acm.graphics.GImage;
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
+
+public class SimplePicture extends GraphicsProgram {
+	public void init() {
+		setSize(500, 500);
+	}
+	
+	public void run() {
+		GImage robot = new GImage("robot.jpg", 200, 100);
+		add(robot);
+		GLabel label = new GLabel("How I feel when I use github", 200, 400);
+		add(label);
+	}
+}
