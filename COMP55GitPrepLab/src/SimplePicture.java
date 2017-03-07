b44d488262ece02aab4b9a59f4ea7de80c1aa47a
Osvaldo Jimenez <ojimenez@pacific.edu>
2017-03-06 16:03:08 -0800

    changed the picture here

--- COMP55GitPrepLab/src/SimplePicture.java
+++ COMP55GitPrepLab/src/SimplePicture.java
@@ -10,7 +10,7 @@ public class SimplePicture extends GraphicsProgram {
 	public void run() {
 		GImage robot = new GImage("robot.jpg", 200, 100);
 		add(robot);
-		GLabel label = new GLabel("How I feel when I use github", 200, 400);
+		GLabel label = new GLabel("How I felt before knowing about github", 200, 300);
 		add(label);
 	}
 }
