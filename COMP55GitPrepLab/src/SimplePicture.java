01ba32796a0c8f0abf5d8e6ea9bb708f12177127
Osvaldo Jimenez <ojimenez@pacific.edu>
2017-03-16 17:31:39 -0700

    added a size constant

--- COMP55GitPrepLab/src/SimplePicture.java
+++ COMP55GitPrepLab/src/SimplePicture.java
@@ -3,8 +3,10 @@ import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
 
 public class SimplePicture extends GraphicsProgram {
+	public static int SIZE = 700;
+	
 	public void init() {
-		setSize(500, 500);
+		setSize(SIZE, SIZE);
 	}
 	
 	public void run() {
