bcabd55158acf5a19e87e812f2f06a6f8ba5ee9e
Osvaldo <ojimenez@pacific.edu>
2017-03-18 14:44:51 -0700

    hackslash

--- COMP55GitPrepLab/src/SimplePicture.java
+++ COMP55GitPrepLab/src/SimplePicture.java
@@ -14,7 +14,7 @@ public class SimplePicture extends GraphicsProgram {
 	public void run() {
 		GImage robot = new GImage("robot.jpg", 200, 100);
 		add(robot);
-		GLabel label = new GLabel("How I feel when using github sometimes", 200, 400);
+		GLabel label = new GLabel("How I feel when using github hackslash", 200, 400);
 		add(label);
 	}
 }
