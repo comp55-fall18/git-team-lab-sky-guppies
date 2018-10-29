6584242b898f1093f4e246825a04f4de76c20f16
mkhan17 <m_khan17@u.pacific.edu>
2018-10-29 10:42:10 -0700

    Hakslash

--- COMP55GitTeamLab/src/SimplePicture.java
+++ COMP55GitTeamLab/src/SimplePicture.java
@@ -13,7 +13,7 @@ public class SimplePicture extends GraphicsProgram {
 	public void run() {
 		GImage robot = new GImage("robot.jpg", 200, 100);
 		add(robot);
-		GLabel label = new GLabel("How We feel when using github hackslah", 200, 400);
+		GLabel label = new GLabel("How We feel when using github hakslah", 200, 400);
 		add(label);
 		
 		GLabel label2 = new GLabel("We need to get help finishing this lab", 300, 500);
